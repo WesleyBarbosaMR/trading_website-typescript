b210aa0005afc3637f7e91418bfa0ab096d8b3d1
Wesley Barbosa <wesleybarbosa.mr26@gmail.com>
2022-06-24 08:54:13 -0300

    Changing Spread by ReadonlyArray to protect list

--- app/models/trades_list_model.ts
+++ app/models/trades_list_model.ts
@@ -7,8 +7,9 @@ export class TradesList {
         this.trades.push(trade);
     }
 
-    listTrades(): Array<Trade> {
-        return [...this.trades];// [...<elements>](spread) add each element of the list in a array to prevent undue changes to data.
+    listTrades(): ReadonlyArray<Trade> {
+        return this.trades;//ReadonlyArray removes the need to create a vector with the list instances, as it already protects the array from being changed through TypeScript properties.
+        //return [...this.trades];// [...<elements>](spread) add each element of the list in a array to prevent undue changes to data.
     }
 }
 
--- dist/js/models/trades_list_model.js
+++ dist/js/models/trades_list_model.js
@@ -6,7 +6,8 @@ export class TradesList {
         this.trades.push(trade);
     }
     listTrades() {
-        return [...this.trades]; // [...<elements>](spread) add each element of the list in a array to prevent undue changes to data.
+        return this.trades; //ReadonlyArray removes the need to create a vector with the list instances, as it already protects the array from being changed through TypeScript properties.
+        //return [...this.trades];// [...<elements>](spread) add each element of the list in a array to prevent undue changes to data.
     }
 }
 /*const trades = new TradesList();
